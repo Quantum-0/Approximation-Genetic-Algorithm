4a77eef800b92f97f9a34f3fa7a41bd0abcc28f7
Антон Куренков <quantum0@mail.ru>
2017-09-15 09:52:02 +0300

    Пофиксил зависимости, удалил лишние ссылки

--- GenAlgTestGui/Chromosome.cs
+++ GenAlgTestGui/Chromosome.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GenAlgTest
+namespace GenAlgTestGui
 {
     /// <summary>
     /// Хромосома - представляет собой экземпляр решения
--- GenAlgTestGui/Population.cs
+++ GenAlgTestGui/Population.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GenAlgTest
+namespace GenAlgTestGui
 {
     // TODO:
     // заменить NextGeneration на отдельные этапы
